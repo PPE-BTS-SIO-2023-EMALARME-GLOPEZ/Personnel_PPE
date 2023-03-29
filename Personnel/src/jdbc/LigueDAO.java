76b3e4a07d954b8ab845603deea9d8dc30e084f4
glopez <glopez-dev@proton.me>
2023-03-29 20:26:29 +0200

    Ajoute le code permettant de récupérer l'id de la ligue une fois que celle ci à été insérée en base de donnée avec la méthode insert()

--- Personnel/src/jdbc/LigueDAO.java
+++ Personnel/src/jdbc/LigueDAO.java
@@ -21,8 +21,30 @@ public class LigueDAO {
         String requete = "INSERT INTO ligue (nom_ligue, administrateur) VALUES (?, ?)";
         PreparedStatement instruction = connection.prepareStatement(requete);
         instruction.setString(1, ligue.getNom());
-        // instruction.setString(2, ligue.getAdministrateur().getId());
-        // instruction.executeUpdate();
+        instruction.setInt(2, ligue.getAdministrateur().getId());
+        instruction.executeUpdate();
+
+        ResultSet generatedKeys = instruction.getGeneratedKeys();
+        if (generatedKeys.next()) {
+            int id = generatedKeys.getInt(1);
+            ligue.setId(id);
+        }
+    }
+
+    public void update(Ligue ligue) throws SQLException {
+        String requete = "UPDATE ligue SET nom = ?, administrateur_id = ? WHERE id = ?";
+        PreparedStatement instruction = connection.prepareStatement(requete);
+        instruction.setString(1, ligue.getNom());
+        instruction.setInt(2, ligue.getAdministrateur().getId());
+        instruction.setInt(3, ligue.getId());
+        instruction.executeUpdate();
+    }
+
+    public void delete(Ligue ligue) throws SQLException {
+        String requete = "DELETE FROM ligue WHERE id = ?";
+        PreparedStatement instruction = connection.prepareStatement(requete);
+        instruction.setInt(1, ligue.getId());
+        instruction.executeUpdate();
     }
 
 }
