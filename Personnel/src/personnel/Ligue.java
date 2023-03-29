d48bb5173fb3e0fc399c88f2c2fa745d012fcbf0
glopez <glopez-dev@proton.me>
2023-03-29 20:10:19 +0200

    Ajout des accesseurs pour la propriété id de la classe Ligue

--- Personnel/src/personnel/Ligue.java
+++ Personnel/src/personnel/Ligue.java
@@ -10,13 +10,12 @@ import java.util.TreeSet;
 /**
  * Représente une ligue. Chaque ligue est reliée à une liste
  * d'employés dont un administrateur. Comme il n'est pas possible
- * de créer un employé sans l'affecter à une ligue, le root est 
- * l'administrateur de la ligue jusqu'à ce qu'un administrateur 
+ * de créer un employé sans l'affecter à une ligue, le root est
+ * l'administrateur de la ligue jusqu'à ce qu'un administrateur
  * lui ait été affecté avec la fonction {@link #setAdministrateur}.
  */
 
-public class Ligue implements Serializable, Comparable<Ligue>
-{
+public class Ligue implements Serializable, Comparable<Ligue> {
 	private static final long serialVersionUID = 1L;
 	@SuppressWarnings("unused")
 	private int id = -1;
@@ -24,20 +23,19 @@ public class Ligue implements Serializable, Comparable<Ligue>
 	private SortedSet<Employe> employes;
 	private Employe administrateur;
 	private GestionPersonnel gestionPersonnel;
-	
+
 	/**
 	 * Crée une ligue.
+	 * 
 	 * @param nom le nom de la ligue.
 	 */
-	
-	public Ligue(GestionPersonnel gestionPersonnel, String nom) throws SauvegardeImpossible
-	{
+
+	public Ligue(GestionPersonnel gestionPersonnel, String nom) throws SauvegardeImpossible {
 		this(gestionPersonnel, -1, nom);
-		this.id = gestionPersonnel.insert(this); 
+		this.id = gestionPersonnel.insert(this);
 	}
 
-	Ligue(GestionPersonnel gestionPersonnel, int id, String nom)
-	{
+	Ligue(GestionPersonnel gestionPersonnel, int id, String nom) {
 		this.nom = nom;
 		employes = new TreeSet<>();
 		this.gestionPersonnel = gestionPersonnel;
@@ -47,44 +45,44 @@ public class Ligue implements Serializable, Comparable<Ligue>
 
 	/**
 	 * Retourne le nom de la ligue.
+	 * 
 	 * @return le nom de la ligue.
 	 */
 
-	public String getNom()
-	{
+	public String getNom() {
 		return nom;
 	}
 
 	/**
 	 * Change le nom.
+	 * 
 	 * @param nom le nouveau nom de la ligue.
 	 */
 
-	public void setNom(String nom)
-	{
+	public void setNom(String nom) {
 		this.nom = nom;
 	}
 
 	/**
 	 * Retourne l'administrateur de la ligue.
+	 * 
 	 * @return l'administrateur de la ligue.
 	 */
-	
-	public Employe getAdministrateur()
-	{
+
+	public Employe getAdministrateur() {
 		return administrateur;
 	}
 
 	/**
 	 * Fait de administrateur l'administrateur de la ligue.
-	 * Lève DroitsInsuffisants si l'administrateur n'est pas 
-	 * un employé de la ligue ou le root. Révoque les droits de l'ancien 
+	 * Lève DroitsInsuffisants si l'administrateur n'est pas
+	 * un employé de la ligue ou le root. Révoque les droits de l'ancien
 	 * administrateur.
+	 * 
 	 * @param administrateur le nouvel administrateur de la ligue.
 	 */
-	
-	public void setAdministrateur(Employe administrateur)
-	{
+
+	public void setAdministrateur(Employe administrateur) {
 		Employe root = gestionPersonnel.getRoot();
 		if (administrateur != root && administrateur.getLigue() != this)
 			throw new DroitsInsuffisants();
@@ -93,16 +91,15 @@ public class Ligue implements Serializable, Comparable<Ligue>
 
 	/**
 	 * Retourne les employés de la ligue.
+	 * 
 	 * @return les employés de la ligue dans l'ordre alphabétique.
 	 */
-	
-	public SortedSet<Employe> getEmployes()
-	{
+
+	public SortedSet<Employe> getEmployes() {
 		return Collections.unmodifiableSortedSet(employes);
 	}
-	
-	public ArrayList<Employe> getEmployes1()
-	{
+
+	public ArrayList<Employe> getEmployes1() {
 		ArrayList<Employe> employesList = new ArrayList<>();
 		for (Employe employe : employes)
 			employesList.add(employe);
@@ -110,13 +107,14 @@ public class Ligue implements Serializable, Comparable<Ligue>
 	}
 
 	/**
-//	 * Ajoute un employé dans la ligue. Cette méthode 
+	 * // * Ajoute un employé dans la ligue. Cette méthode
 	 * est le seul moyen de créer un employé.
-	 * @param nom le nom de l'employé.
-	 * @param prenom le prénom de l'employé.
-	 * @param mail l'adresse mail de l'employé.
+	 * 
+	 * @param nom      le nom de l'employé.
+	 * @param prenom   le prénom de l'employé.
+	 * @param mail     l'adresse mail de l'employé.
 	 * @param password le password de l'employé.
-	 * @return l'employé créé. 
+	 * @return l'employé créé.
 	 */
 
 	public Employe addEmploye(String nom, String prenom, String mail, String password) // Contrat en CDI
@@ -126,38 +124,55 @@ public class Ligue implements Serializable, Comparable<Ligue>
 		return employe;
 	}
 
-	public Employe addEmploye(String nom, String prenom, String mail, String password, LocalDate dateFinContrat) // Contrat en CDI
+	public Employe addEmploye(String nom, String prenom, String mail, String password, LocalDate dateFinContrat) // Contrat
+																													// en
+																													// CDI
 	{
 		Employe employe = new Employe(this.gestionPersonnel, this, nom, prenom, mail, password, dateFinContrat);
 		employes.add(employe);
 		return employe;
 	}
-	
-	public void removeEmploye(Employe employe)
-	{
+
+	public void removeEmploye(Employe employe) {
 		employes.remove(employe);
 	}
-		
+
 	/**
 	 * Supprime la ligue, entraîne la suppression de tous les employés
 	 * de la ligue.
 	 */
-	
-	public void remove()
-	{
+
+	public void remove() {
 		gestionPersonnel.remove(this);
 	}
-	
+
+	/**
+	 * Retourne l'ID de la ligue
+	 * 
+	 * @return l'ID de la ligue
+	 */
+	public int getId() {
+		return this.id;
+	}
+
+	/**
+	 * Fixe l'ID de la ligue, cet ID doit être celui retourné par JDBC
+	 * après enregistrement de la ligue dans la base de donnée.
+	 * Cette méthode ne doit être utilisée que par la classe LigueDAO
+	 * 
+	 * @param id l'identifiant de la ligue en base de donnée
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
 
 	@Override
-	public int compareTo(Ligue autre)
-	{
+	public int compareTo(Ligue autre) {
 		return getNom().compareTo(autre.getNom());
 	}
-	
+
 	@Override
-	public String toString()
-	{
+	public String toString() {
 		return nom;
 	}
 }
