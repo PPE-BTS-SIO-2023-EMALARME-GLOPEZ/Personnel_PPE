259c73e6f368884b79b3cf5612078a975f9ac39e
glopez <glopez-dev@proton.me>
2023-03-29 17:38:11 +0200

    Connexion à JDBC

--- create_personnel.sql
+++ create_personnel.sql
@@ -1,11 +1,8 @@
-
-DROP DATABASE IF EXISTS personnel; 
-CREATE DATABASE personnel ;
+DROP DATABASE IF EXISTS personnel;
+CREATE DATABASE personnel;
 USE personnel;
-
-
 CREATE TABLE employe (
-  `id_employe` BIGINT,
+  `id_employe` BIGINT NOT NULL AUTO_INCREMENT,
   `nom_employe` VARCHAR(50),
   `prenom_employe` VARCHAR(50),
   `password` VARCHAR(50),
@@ -14,14 +11,15 @@ CREATE TABLE employe (
   `date_depart` DATE,
   `id_ligue` BIGINT,
   PRIMARY KEY (`id_employe`)
-) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
-
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8MB4;
 CREATE TABLE ligue (
-  `id_ligue` BIGINT,
+  `id_ligue` BIGINT NOT NULL AUTO_INCREMENT,
   `nom_ligue` VARCHAR(42),
   `administrateur` BIGINT,
   PRIMARY KEY (`id_ligue`)
-) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
-
-ALTER TABLE `employe` ADD FOREIGN KEY (`id_ligue`) REFERENCES `ligue` (`id_ligue`);
-ALTER TABLE `ligue` ADD FOREIGN KEY (`administrateur`) REFERENCES `employe` (`id_employe`);
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8MB4;
+ALTER TABLE `employe`
+ADD FOREIGN KEY (`id_ligue`) REFERENCES `ligue` (`id_ligue`);
+ALTER TABLE `ligue`
+ADD FOREIGN KEY (`administrateur`) REFERENCES `employe` (`id_employe`);
+GRANT ALL PRIVILEGES ON personnel TO passerelle @localhost;
