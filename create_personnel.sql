35773ecd32e6b3dababb0f6c6ef66e085e2d79d5
glopez <glopez-dev@proton.me>
2023-03-31 21:46:22 +0200

    Ajout d'un delete en cascade des employes des ligues

--- create_personnel.sql
+++ create_personnel.sql
@@ -1,6 +1,9 @@
 DROP DATABASE IF EXISTS personnel;
+
 CREATE DATABASE personnel;
+
 USE personnel;
+
 CREATE TABLE employe (
   `id_employe` BIGINT NOT NULL AUTO_INCREMENT,
   `nom_employe` VARCHAR(50),
@@ -12,14 +15,22 @@ CREATE TABLE employe (
   `id_ligue` BIGINT,
   PRIMARY KEY (`id_employe`)
 ) ENGINE = InnoDB DEFAULT CHARSET = UTF8MB4;
+
 CREATE TABLE ligue (
   `id_ligue` BIGINT NOT NULL AUTO_INCREMENT,
   `nom_ligue` VARCHAR(42),
   `administrateur` BIGINT,
   PRIMARY KEY (`id_ligue`)
 ) ENGINE = InnoDB DEFAULT CHARSET = UTF8MB4;
-ALTER TABLE `employe`
-ADD FOREIGN KEY (`id_ligue`) REFERENCES `ligue` (`id_ligue`);
-ALTER TABLE `ligue`
-ADD FOREIGN KEY (`administrateur`) REFERENCES `employe` (`id_employe`);
+
+ALTER TABLE
+  `employe`
+ADD
+  CONSTRAINT `FK_employe_ligue` FOREIGN KEY (`id_ligue`) REFERENCES `ligue` (`id_ligue`) ON DELETE CASCADE;
+
+ALTER TABLE
+  `ligue`
+ADD
+  FOREIGN KEY (`administrateur`) REFERENCES `employe` (`id_employe`);
+
 GRANT ALL PRIVILEGES ON personnel TO passerelle @localhost;
